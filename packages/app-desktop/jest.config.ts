359d53876f8075494c66f018adac8fbbbcd331e1
nielsdelestinne <nielsdelestinne@gmail.com>
2022-06-23 20:28:35 +0200

    Rename desktop to app-desktop & remove e2eˆ

--- packages/app-desktop/jest.config.ts
+++ packages/app-desktop/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'desktop',
+  displayName: 'app-desktop',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   transform: {
     '^.+\\.[tj]s$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/desktop',
+  coverageDirectory: '../../coverage/packages/app-desktop',
 };
