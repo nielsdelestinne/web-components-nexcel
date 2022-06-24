2a2a4fc97b6c9e75f8412741cb8edffa0ea901e1
nielsdelestinne <nielsdelestinne@gmail.com>
2022-06-24 11:51:57 +0200

    Button onClick added

--- packages/nexcel/src/app/features/home/home.element.ts
+++ packages/nexcel/src/app/features/home/home.element.ts
@@ -4,6 +4,10 @@ import './home.element.scss';
 @defineCustomElement('nexcel-home')
 export class HomeElement extends HTMLElement {
 
+  constructor(private identifier: string = 'homeElement') {
+    super();
+    this.id = this.identifier;
+  }
   connectedCallback() {
     const title = 'Nexcel';
     const subTitle = 'The next Excel';
@@ -11,13 +15,21 @@ export class HomeElement extends HTMLElement {
       <section id="home" class="flex-container">
         <div class="flex-item">
           <h1>${title} <br> <span>${subTitle}</span></h1>
-          <div class="flex-container">
-              <button class="flex-item button-new" id="create-new">New</button>
-              <button class="flex-item button-load">Load</button>
+          <div class="flex-container shadow-root">
+              <button class="flex-item button-new" id="create-new" onclick="${this.identifier}.createNew()">New</button>
+              <button class="flex-item button-load" onclick="${this.identifier}.loadExisting()">Load</button>
           </div>
         </div>
       </section>
     `;
+  }
+
+  createNew() {
+    alert('new');
+  }
+
+  loadExisting() {
+    alert('existing');
   }
 
 }
